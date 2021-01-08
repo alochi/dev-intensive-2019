a0d360773fda874a453456fdd246146d462eedba
alochi <ochirovaldar@gmail.com>
2021-01-08 19:38:44 +0300

    hometask_3 complete 2

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -14,27 +14,35 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> =
         if (question.answers.contains(answer)) {
             question = question.nextQuestion()
-            "Отлично - это правильный ответ!\n${question.question}" to status.color
+            "Отлично - ты справился\n${if (question != Question.IDLE) question.question else "На этом все, вопросов больше нет"}" to status.color
         } else {
             status = status.nextStatus()
-            "Это не правильный ответ\n${question.question}" to status.color
+            if (status != Status.NORMAL)
+                "Это не правильный ответ\n${question.question}" to status.color
+            else {
+                question = Question.NAME
+                "Это не правильный ответ. Давай все по новой\n${question.question}" to status.color
+            }
         }
 
     enum class Status(val color: Triple<Int, Int, Int>) {
         NORMAL(Triple(255, 255, 255)),
-        WARNING(Triple(255, 255, 0)),
-        DANGER(Triple(255, 120, 0)),
-        CRITICAL(Triple(255, 60, 60));
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
 
         fun nextStatus(): Status =
-            if (this.ordinal < values().lastIndex) values()[this.ordinal + 1] else values()[0]
+            if (this.ordinal < values().lastIndex)
+                values()[this.ordinal + 1]
+            else
+                values()[0]
     }
 
     enum class Question(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
-        PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")) {
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
